002247e26998023c8b29470fa33499ec477c8b90
David Thurman <davidthurmanwork@gmail.com>
2017-09-01 10:40:30 -0400

    Initial commit

--- CustomEmailLogin/ViewController.swift
+++ CustomEmailLogin/ViewController.swift
@@ -7,9 +7,20 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var signInSelector: UISegmentedControl!
+    @IBOutlet weak var signInLabel: UILabel!
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var signInButton: UIButton!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    var isSignIn:Bool = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +31,59 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func signInAction(_ sender: Any) {
+        
+        if let email = emailTextField.text, let pass = passwordTextField.text {
+            if isSignIn {
+                Auth.auth().signIn(withEmail: email, password: pass, completion: { (user, error) in
+                    
+                    //Check that user isnt nil
+                    if let u = user {
+                        //User is found, go to home screen
+                        self.performSegue(withIdentifier: "gotohome", sender: self)
+                    }
+                    else {
+                        //Error: check error and show message
+                    }
+                })
+            }
+            else {
+                Auth.auth().createUser(withEmail: email, password: pass) { (user, error) in
+                    if let u = user {
+                        print("TEST1")
+                        //Registered successfully
+                        self.performSegue(withIdentifier: "gotohome", sender: self)
+                    }
+                    else {
+                        print("TEST2")
+                        //Error: check error and show message
+                        print(error)
+                    }
+                }
+            }
+        }
 
+    }
+
+    @IBAction func signInSelectorChanged(_ sender: UISegmentedControl) {
+        
+        isSignIn = !isSignIn
+        
+        if isSignIn {
+            signInLabel.text = "Sign In"
+            signInButton.setTitle("Sign In", for: .normal)
+        }
+        else {
+            signInLabel.text = "Register"
+            signInButton.setTitle("Register", for: .normal)
+        }
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        emailTextField.resignFirstResponder()
+        passwordTextField.resignFirstResponder()
+    }
+    
 }
 
